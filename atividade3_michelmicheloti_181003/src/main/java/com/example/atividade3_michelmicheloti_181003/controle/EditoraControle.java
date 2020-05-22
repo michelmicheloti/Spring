d07cd3b62536293d6a3f025922b52a0607d17397
michelmicheloti <michel_michelot@hotmail.com>
2020-05-22 20:36:13 -0300

    Feito a associação de livros com editoras

--- atividade3_michelmicheloti_181003/src/main/java/com/example/atividade3_michelmicheloti_181003/controle/EditoraControle.java
+++ atividade3_michelmicheloti_181003/src/main/java/com/example/atividade3_michelmicheloti_181003/controle/EditoraControle.java
@@ -1,7 +1,11 @@
 package com.example.atividade3_michelmicheloti_181003.controle;
 
+import java.util.List;
+
 import com.example.atividade3_michelmicheloti_181003.entidade.Editoras;
+import com.example.atividade3_michelmicheloti_181003.entidade.Livros;
 import com.example.atividade3_michelmicheloti_181003.servico.editoraServico;
+import com.example.atividade3_michelmicheloti_181003.servico.livroServico;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,27 +13,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EditoraControle {
     @Autowired
     private editoraServico editoraServico;
+
+    @Autowired
+    private livroServico livroServico;
     
     @GetMapping("/editoras")
     public ModelAndView getEditoras(){
         ModelAndView mv = new ModelAndView("editorasTemplate");
-        mv.addObject("editoras", editoraServico.getEditoras());        
-        return mv;
-    }
 
-    @GetMapping("/detalhesEditora/{codigo}")
-    public ModelAndView getEditoraDetalhes(@PathVariable(name = "codigo") Integer codigo) {
-        
-        Editoras editora = editoraServico.getEditorasById(codigo);
-        ModelAndView mv = new ModelAndView("detalhesEditora");
-        mv.addObject("editora", editora);
-
+        mv.addObject("editora", new Editoras());
+        mv.addObject("editoras", editoraServico.getEditoras());   
+        mv.addObject("livros", livroServico.getLivros());
+             
         return mv;
     }
 
@@ -37,5 +39,34 @@ public class EditoraControle {
     public String salvar(@ModelAttribute Editoras editoras){
         editoraServico.salvar(editoras);
         return "redirect:/editoras";
+    }
+
+    
+    @PostMapping("/associarLivroEditora")
+    public String associarLivro(@ModelAttribute Livros livro, @RequestParam Integer codigoEditora) {
+        
+
+        Editoras editoras = editoraServico.getEditorasById(codigoEditora);
+        livro = livroServico.getLivrosById(livro.getCodigo());
+        
+
+        editoras.getLivros().add(livro);
+        editoraServico.salvar(editoras);
+
+        return "redirect:/detalhesEditora/" + codigoEditora;
+    }
+
+    @GetMapping("/detalhesEditora/{codigo}")
+    public ModelAndView getEditoraDetalhes(@PathVariable(name = "codigo") Integer codigo) {
+
+        Editoras editora = editoraServico.getEditorasById(codigo);
+        ModelAndView mv = new ModelAndView("detalhesEditora");
+        mv.addObject("editora", editora);
+        
+        List <Livros> livrosNaoAssociados = livroServico.getLivros();
+        livrosNaoAssociados.removeAll(editora.getLivros());
+        mv.addObject("livros", livrosNaoAssociados);
+
+        return mv;
     }
 }
